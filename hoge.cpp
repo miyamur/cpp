3a4a9c88343ce62e9e179a28d82c770e709ad5ad
Kazushi Miyamura <miyamura@cyber.t.u-tokyo.ac.jp>
2019-06-24 21:02:48 +0900

    コンストラクタとデストラクタのトレーニング

---
+++ hoge.cpp
@@ -0,0 +1,17 @@
+#include "hoge.h"
+#include <iostream>
+
+using namespace std;
+
+void Hoge::foo()
+{
+    cout << "fooメソッド" << endl;
+}
+
+Hoge::Hoge(){
+  cout<<"コンストラクタ"<<endl;
+}
+
+Hoge::~Hoge(){
+  cout<<"デストラクタ"<<endl;
+}
